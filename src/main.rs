a4343444f7a85bb2948a3558290225879d90b7ad
smlc <siesa.marlon@hotmail.fr>
2022-12-01 22:58:42 +0000

    Solution for the 3 elf with the most calories

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let calories = include_str!("input.txt");
-    let max = calories
+    let mut calories_by_elf : Vec<usize> = calories
         .split("\n\n")
         .map(|elf_calories|{
             return elf_calories
@@ -8,7 +8,13 @@ fn main() {
                 .flat_map(str::parse::<usize>)
                 .sum::<usize>();
         })
-        .max().unwrap();
+        .collect();
 
+        calories_by_elf.sort_by(|a, b| b.cmp(a));
+        //print!("{:?}", calories_by_elf);
+        let max : usize = calories_by_elf.iter()
+        .take(3)
+        .sum();
+        
         print!("Max is {max}");
 }
